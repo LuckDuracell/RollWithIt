30507ea393acfd4899ee70efd6d95ad88fe40f2d
LuckDev <lukedru@yahoo.com>
2023-01-24 23:27:37 -0800

    Initial Commit

---
+++ RollWithIt/RollWithItApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollWithItApp.swift
+//  RollWithIt
+//
+//  Created by Luke Drushell on 1/24/23.
+//
+
+import SwiftUI
+
+@main
+struct RollWithItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
